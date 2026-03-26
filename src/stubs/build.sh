5012011962b9dcbc2ecbc289ec1d8aa2806362a3
Aadish Verma <mail@aadishv.dev>
2026-03-26 15:00:05 -0700

    screw you too cloudflare

--- src/stubs/build.sh
+++ src/stubs/build.sh
@@ -1,5 +1,6 @@
 # TODO: just use master once `feat/stubs` is merged in
-git clone --depth 1 --no-checkout --single-branch --branch "feat/stubs" https://github.com/venice-v5/venice; cd venice
+git clone --depth 1 --no-checkout --single-branch --branch "feat/stubs" https://github.com/venice-v5/venice
+cd venice
 git sparse-checkout set venice
 git checkout
 cd venice/venice
