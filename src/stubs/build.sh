de5a127a7191703a6e3f94443a4bf778d731f450
Aadish Verma <mail@aadishv.dev>
2026-03-25 23:29:42 -0700

    chore: simplify docsgen

--- src/stubs/build.sh
+++ src/stubs/build.sh
@@ -1,15 +1,7 @@
 # TODO: just use master once `feat/stubs` is merged in
-git clone --depth 1 --no-checkout --single-branch --branch "feat/stubs" https://github.com/venice-v5/venice
-
-cd venice
-
+git clone --depth 1 --no-checkout --single-branch --branch "feat/stubs" https://github.com/venice-v5/venice; cd venice
 git sparse-checkout set venice
-
 git checkout
-
 cd ..
-
-# actually build the docs
-uvx pdoc --search -n -t templates venice/venice/venice/__init__.py -o output
-
+uvx pdoc --search -n -t src/stubs/templates venice/venice/venice/__init__.py -o src/stubs/output
 rm -rf venice
